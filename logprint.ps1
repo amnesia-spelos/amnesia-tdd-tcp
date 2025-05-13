20ac0b5152ce69a744b5e19551c7c7979cbb06ea
petrspelos <petrspelos@users.noreply.github.com>
2025-05-13 11:46:32 +0200

    Extend print script to include messages

--- logprint.ps1
+++ logprint.ps1
@@ -1,6 +1,28 @@
 $logFile = "$HOME\Documents\Amnesia\Main\hpl.log"
 if (Test-Path $logFile) {
-    Get-Content $logFile | Where-Object { $_ -match '^\[amnesia-tdd-tcp\]' } | ForEach-Object { Write-Output $_ }
+    $lines = Get-Content $logFile
+    $printingMessage = $false
+
+    for ($i = 0; $i -lt $lines.Count; $i++) {
+        $line = $lines[$i]
+
+        if ($line -match '^\[amnesia-tdd-tcp\]') {
+            Write-Output $line
+
+            if ($i + 1 -lt $lines.Count -and $lines[$i + 1] -match '^<message>') {
+                $i++
+                do {
+                    Write-Output $lines[$i]
+                    $i++
+                } while ($i -lt $lines.Count -and $lines[$i] -notmatch '^</message>')
+                
+                # Also print the closing tag if found
+                if ($i -lt $lines.Count) {
+                    Write-Output $lines[$i]
+                }
+            }
+        }
+    }
 } else {
     Write-Output "Log file not found: $logFile"
 }
